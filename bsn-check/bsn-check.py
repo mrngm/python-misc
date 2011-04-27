5c08ff05fdfa7ab33c1b20c496278bd8c131035e
mrngm <har@mrngm.com>
2011-04-27 22:40:33 +0200

    eleven test algorithm implemented, not working yet

--- bsn-check/bsn-check.py
+++ bsn-check/bsn-check.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+### bsn-check.py
+### 
+### A small program to check whether a BSN is a valid one, by evaluating
+### the eleven test.
+###
+### This program was written by mrngm
+
 import sys
 
 def print_readme():
@@ -12,9 +19,34 @@ When no number is given, this help message is displayed.
 This script was made by mrngm, source at https://github.com/mrngm/python-misc/
 """
 
+def eleven_test(number):
+	if len(number) > 9:
+		sys.exit("Number is too long, exiting");
+	elif len(number) == 8 and number.isdigit():
+		fullnumber = "0" + number[0:len(number)]
+	elif len(number) == 9 and number.isdigit():
+		fullnumber = number
+	else:
+		sys.exit("Number too short or not a number, exiting");
+	
+	total = 0
+	for i in range(8):
+		total += int(fullnumber[i])*(9-i)
+		print "Fullnumber["+str(i)+"]="+fullnumber[i]+", multiplier: " + str(9-i) + " Total: " + str(total)
+	checkdigit = total / 11
+	print "Check digit raw: "+str(checkdigit)
+	if checkdigit == 10:
+		checkdigit = 0
+	print "BSN: " + fullnumber + ", check digit: " + str(checkdigit)
+	if checkdigit == int(fullnumber[8]):
+		print "BSN is valid!"
+	else:
+		print "BSN is NOT valid."
+
+
 if len(sys.argv) < 2:
 	# print README
 	print_readme()
 elif len(sys.argv) == 2:
 	# do the eleven check
-	print "Eleven check here"
+	eleven_test(sys.argv[1])
