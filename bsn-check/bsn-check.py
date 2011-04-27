4f2310b9f7d1f684b2ed1de9545bc078f81545b6
mrngm <har@mrngm.com>
2011-04-27 22:49:59 +0200

    verbosity option added

--- bsn-check/bsn-check.py
+++ bsn-check/bsn-check.py
@@ -10,16 +10,17 @@
 import sys
 
 def print_readme():
-	print """bsn-check.py [number]
+	print """bsn-check.py -v [number]
 
 Checks whether number is a valid BSN.
+-v			increase verbosity (show steps)
 [number]	BSN to check
 
 When no number is given, this help message is displayed.
 This script was made by mrngm, source at https://github.com/mrngm/python-misc/
 """
 
-def eleven_test(number):
+def eleven_test(number, verbosity):
 	if len(number) > 9:
 		sys.exit("Number is too long, exiting");
 	elif len(number) == 8 and number.isdigit():
@@ -32,9 +33,11 @@ def eleven_test(number):
 	total = 0
 	for i in range(8):
 		total += int(fullnumber[i])*(9-i)
-		print "Fullnumber["+str(i)+"]="+fullnumber[i]+", multiplier: " + str(9-i) + " Total: " + str(total)
+		if verbosity:
+			print "Fullnumber["+str(i)+"]="+fullnumber[i]+", multiplier: " + str(9-i) + " Total: " + str(total)
 	checkdigit = total % 11
-	print "Check digit raw: "+str(checkdigit)+" total: "+str(total)
+	if verbosity:
+		print "Check digit raw: "+str(checkdigit)+" total: "+str(total)
 	if checkdigit == 10:
 		checkdigit = 0
 	print "BSN: " + fullnumber + ", check digit: " + str(checkdigit)
@@ -49,4 +52,11 @@ if len(sys.argv) < 2:
 	print_readme()
 elif len(sys.argv) == 2:
 	# do the eleven check
-	eleven_test(sys.argv[1])
+	eleven_test(sys.argv[1], False)
+elif len(sys.argv) == 3 and sys.argv[1] == "-v":
+	# do the eleven check with increased verbosity
+	print "Verbosity set, increasing verbosity"
+	eleven_test(sys.argv[2], True)
+else:
+	# catchall, print README
+	print_readme()
