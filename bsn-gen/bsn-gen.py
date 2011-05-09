61f25ef7d941faa9022553ad4b25bfbcaf12cccc
mrngm <har@mrngm.com>
2011-05-09 12:07:59 +0200

    Help text for bsn-gen.py added

---
+++ bsn-gen/bsn-gen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+### bsn-gen.py
+### 
+### A small program to generate a number of valid BSNs.
+###
+### This program was written by mrngm
+
+import sys
+
+def print_readme():
+	print """bsn-gen.py [number]
+
+Generates [number] of valid BSNs
+[number]	Number of BSNs to generate
+
+When no number is given, this help message is displayed.
+This script was made by mrngm, source at https://github.com/mrngm/python-misc/
+"""
+
+
