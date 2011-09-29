b9de26f7a09af887d95f88a03aae7cadd5fef39e
mrngm <har@mrngm.com>
2011-09-30 00:18:59 +0200

    Added date functionality

--- etherlocator/etherlocator.py
+++ etherlocator/etherlocator.py
@@ -7,15 +7,17 @@ import re
 import datetime
 
 def print_readme():
-	print """etherlocator.py <mac-addr> [location_file]
+	print """etherlocator.py <mac-addr> [dd/mm] [location_file]
 
 <mac-addr>       The MAC address of the station you want to locate
+[dd/mm]          (optional) The date for lookup
 [location-file]  (optional) File containing all the locations
 """
 
-def request_info(macaddr):
+def request_info(macaddr, dag):
 	today = datetime.date.today()
-	dag = today.strftime('%d/%m')
+	if dag == None:
+		dag = today.strftime('%d/%m')
 	info = { "macaddr": macaddr }
 	etheroutput = commands.getoutput("ethergids " + info['macaddr'])
 	info['ether'] = etheroutput.splitlines()
@@ -36,13 +38,18 @@ def ethergids_to_dict(str):
 	info['ipaddr']  = splitted[2]
 	info['swport']  = splitted[3]
 	if 'ap-huyg' in splitted[3]:
+		print "Die is der"
 		info['loc'] = location_from_ap(splitted[3])
+	else:
+		print "Nee hoor"
+		info['loc'] = splitted[4]
 	info['name']    = splitted[5]
 	return info
 
 def location_from_ap(ap):
 	# standard: read file 'ap_locations.txt'
 	global ap_file
+	aploc = '???'
 	txt = open(ap_file, 'r')
 	splitted = txt.readlines()
 	ap = re.split('/', ap)
@@ -70,12 +77,22 @@ ap_file = 'ap_locations.txt'
 
 if len(sys.argv) < 2:
 	print_readme()
-elif len(sys.argv) >= 2:
-	if len(sys.argv) == 3:
-		ap_file = sys.argv[2]
+elif len(sys.argv) == 2:
 	information = request_info(sys.argv[1])
 	print_info(information)
-	
+elif len(sys.argv) == 3:
+	if len(sys.argv[2]) == 5 and '/' in sys.argv[2]:
+		dag = sys.argv[2] # assuming ap_file != 'ap/bl'
+		information = request_info(sys.argv[1], dag)
+	else:
+		ap_file = sys.argv[2]
+		information = request_info(sys.argv[1], '')
+	print_info(information)
+elif len(sys.argv) == 4:
+	dag = sys.argv[2]
+	ap_file = sys.argv[3]
+	information = request_info(sys.argv[1], dag)
+	print_info(information)
 
 
 # vim: set background=dark:
