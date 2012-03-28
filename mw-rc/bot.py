d890822ccf4fc8d1025ff450af627e45c8be380c
mrngm <har@mrngm.com>
2012-03-28 16:26:15 +0200

    Added message for source

--- mw-rc/bot.py
+++ mw-rc/bot.py
@@ -50,6 +50,8 @@ while 1:
 			if (len(line) > 2):
 				if(line[1]=="NOTICE" and line[2] == "Auth" and line[3] == ":Welcome"):
 					auth=1
+				if(line[3]==":WikiRC:" and line[4] == "src"):
+					s.send("PRIVMSG " + CHANNEL + " :src at https://github.com/mrngm/python-misc/tree/master/mw-rc/\r\n")
 			print line
 
 		if (joined == 0 and auth == 1):
